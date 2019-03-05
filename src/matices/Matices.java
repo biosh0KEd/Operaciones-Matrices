4e5e4df0e6d0229559c234549963aad028f7626f
Efrain Giron <efrainbond@hotmail.com>
2019-03-05 13:23:27 -0600

    Creado el menu de inicio

--- src/matices/Matices.java
+++ src/matices/Matices.java
@@ -5,6 +5,7 @@
  */
 package matices;
 
+import java.util.Scanner;
 /**
  *
  * @author Efraín
@@ -15,7 +16,27 @@ public class Matices {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entDatos;
+        entDatos = new Scanner(System.in);
+        System.out.println("Operaciones con matrices");
+        System.out.println("Escoje una opcion:");
+        System.out.println("1:Suma\n2:Resta\n3:Multiplicación\n4:Transpuesta");
+        System.out.println("5:Inversa\n6:Determinante");
+        int resp = entDatos.nextInt();
+        switch(resp) {
+            case 1:
+            break;
+            case 2:
+            break;
+            case 3:
+            break;
+            case 4:
+            break;
+            case 5:
+            break;
+            case 6:
+            break;
+        }
     }
     
 }
