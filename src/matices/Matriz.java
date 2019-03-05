78eeaba1a8a3a2688f0b12c1b1f1b6968d400f9d
Efrain Giron <efrainbond@hotmail.com>
2019-03-05 13:29:55 -0600

    Creada la clase Matriz

---
+++ src/matices/Matriz.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matices;
+
+/**
+ *
+ * @author Efraín
+ */
+public class Matriz {
+    int[][] datos;
+    public Matriz(int colums, int rows){
+        datos = new int[colums][rows];
+    }
+}
